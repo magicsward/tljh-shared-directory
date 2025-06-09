be8341a81d63246c87b4ca2ccc96b36412d9815c
magicsward <135935532+magicsward@users.noreply.github.com>
2025-06-09 18:23:12 +0800

    Update tljh_shared_directory.py

--- tljh_shared_directory.py
+++ tljh_shared_directory.py
@@ -4,26 +4,26 @@ import sh
 
 @hookimpl
 def tljh_extra_user_conda_packages():
-    return ['voila']
+    return ['Done']
 
 @hookimpl
 def tljh_config_post_install(config):
     """
-    Configure /srv/scratch and change configs/mods
+    Configure /srv/Shared_Data and change configs/mods
     """
     ### mkdir -p /srv/scratch
     ### sudo chown  root:jupyterhub-users /srv/scratch
     ### sudo chmod 777 /srv/scratch
     ### sudo chmod g+s /srv/scratch
     ### sudo ln -s /srv/scratch /etc/skel/scratch
-    sh.mkdir('/srv/scratch', '-p')
+    sh.mkdir('/mnt/Shared_Data', '-p')
     # jupyterhub-users doesn't get created until a user logs in
     # make sure it's created before changing permissions on directory
     ensure_group('jupyterhub-users') 
-    sh.chown('root:jupyterhub-users', '/srv/scratch')
-    sh.chmod('777', '/srv/scratch')
-    sh.chmod('g+s', '/srv/scratch')
-    sh.ln('-s', '/srv/scratch', '/etc/skel/scratch')
+    sh.chown('root:jupyterhub-users', '/mnt/Shared_Data')
+    sh.chmod('777', '/mnt/Shared_Data')
+    sh.chmod('g+s', '/mnt/Shared_Data')
+    sh.ln('-s', '/mnt/Shared_Data', '/etc/skel/Shared_Data')
 
     
     
